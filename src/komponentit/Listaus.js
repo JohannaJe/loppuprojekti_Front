f19304400ef13ea50501bb754a10d18195cfafa7
joniketo <joni.m.keto@gmail.com>
2018-04-03 16:12:52 +0300

    mappays ja feediolioiden printtaus sivulle listana

--- src/komponentit/Listaus.js
+++ src/komponentit/Listaus.js
@@ -5,11 +5,25 @@ import './Listaus.css';
 export class Listaus extends React.Component {
 
     render() {
-        var kaikki = this.props.lista;
+        var kaikki = this.props.lista.map(function(x) {
+            return(
+                <div>
+                    {x.otsikko}
+                    {x.linkki}
+                    {x.aikaleima}
+                <br/>
+                    <br/>
+
+                </div>
+            )
+
+        })
+
         return (
             <div className="listausreuna">
                 <p>
                     {kaikki}
+
                 </p>
             </div>
         );
