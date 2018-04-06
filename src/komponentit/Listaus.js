cc1b46ae365df22b85a816318a509f885f7683d5
joniketo <joni.m.keto@gmail.com>
2018-04-06 12:17:51 +0300

    listaus ja navbar juttuja, linkin värin vaihto ja hampurilais iconi

--- src/komponentit/Listaus.js
+++ src/komponentit/Listaus.js
@@ -7,10 +7,10 @@ export class Listaus extends React.Component {
     render() {
         var kaikki = this.props.lista.map(function(x, index) {
             return(
-                <div key={index}>
-                    <p>{x.aikaleimaString}</p>
-                    <p><a href={x.linkki}><img className="img-responsive img-thumbnail center-block" src={x.kuva} width="460" height="345" /></a></p>
-                    <p><a href={x.linkki}>{x.otsikko}</a></p>
+                <div  key={index}>
+                    <p class="aikaleima">{x.aikaleimaString}</p>
+                    <p ><a href={x.linkki} ><img className="img-responsive img-thumbnail center-block" src={x.kuva} width="460" height="345" /></a></p>
+                    <p ><a class="linkki" href={x.linkki} >{x.otsikko}</a></p>
 
                 <br/>
                     <br/>
--- src/komponentit/NaviBar.js
+++ src/komponentit/NaviBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export class NaviBar extends React.Component {
     render() {
@@ -9,29 +9,40 @@ export class NaviBar extends React.Component {
 
                 <nav class="navbar navbar-inverse">
                     <div class="container-fluid">
+
                         <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainNavBar">
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
                             <a class="navbar-brand" href="#">Need4Feed</a>
                         </div>
-                        <ul class="nav navbar-nav">
-                            <li><Link to={"/home"}>Home</Link></li>
-                            <li><a href="#">My Page</a></li>
-                            <li><a href="#">Something</a></li>
-                        </ul>
-                        <form className="navbar-form navbar-left" action="/action_page.php">
-                            <div className="form-group">
-                                <input type="text" className="form-control" placeholder="Search" name="search"></input>
-                            </div>
-                            <button type="submit" className="btn btn-default">
-                                <i className="glyphicon glyphicon-search"></i></button>
-                        </form>
-                        <ul class="nav navbar-nav navbar-right">
-                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                            <li><Link to={"/login"}>Login</Link></li>
-                            <li><Link to={"/register"}>Register</Link></li>
-                        </ul>
+
+                        <div class="collapse navbar-collapse" id="mainNavBar">
+                            <ul class="nav navbar-nav">
+                                <li><Link to={"/home"}>Home</Link></li>
+                                <li><a href="#">My Page</a></li>
+                                <li><a href="#">Something</a></li>
+                            </ul>
+
+
+                            <form className="navbar-form navbar-left" action="/action_page.php">
+                                <div className="form-group">
+                                    <input type="text" className="form-control" placeholder="Search"
+                                           name="search"></input>
+                                </div>
+                                <button type="submit" className="btn btn-default">
+                                    <i className="glyphicon glyphicon-search"></i></button>
+                            </form>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                                <li><Link to={"/login"}>Login</Link></li>
+                                <li><Link to={"/register"}>Register</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
-
 
 
             </div>
