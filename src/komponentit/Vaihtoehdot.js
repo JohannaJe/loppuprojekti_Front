a3d210cd1bb3feb562526cd8064fed22efbdef66
joniketo <joni.m.keto@gmail.com>
2018-04-11 13:53:46 +0300

    vaihtoehdot js options:valuet asetettu

--- src/komponentit/Vaihtoehdot.js
+++ src/komponentit/Vaihtoehdot.js
@@ -33,14 +33,18 @@ export class Vaihtoehdot extends React.Component {
 
 
 
-                    <select id="example-getting-started" multiple="multiple">
-                        <option value="cheese">Cheese</option>
-                        <option value="tomatoes">Tomatoes</option>
-                        <option value="mozarella">Mozzarella</option>
-                        <option value="mushrooms">Mushrooms</option>
-                        <option value="pepperoni">Pepperoni</option>
-                        <option value="onions">Onions</option>
-                    </select>
+                    {/*<select id="example-getting-started" multiple="multiple">*/}
+                        {/*<option value="cheese">Cheese</option>*/}
+                        {/*<option value="tomatoes">Tomatoes</option>*/}
+                        {/*<option value="mozarella">Mozzarella</option>*/}
+                        {/*<option value="mushrooms">Mushrooms</option>*/}
+                        {/*<option value="pepperoni">Pepperoni</option>*/}
+                        {/*<option value="onions">Onions</option>*/}
+                    {/*</select>*/}
+                    <b>URL</b>: <input type="text" onChange={this.props.URL}></input>
+                    <button className="btn btn-info" onClick={this.props.haefunktio}>Search</button>
+                    <button className="myBtn" onClick={this.topFunction} title="Go to top"><i class="icon-chevron-up"/>
+                    </button>
 
                     <table className="logo-rivi">
                         <tr>
@@ -71,10 +75,10 @@ export class Vaihtoehdot extends React.Component {
 
                                     <select className="mdb-select">
                                         <option value="" disabled selected>Pick your favorites</option>
-                                        <option value="1">World</option>
-                                        <option value="2">Business</option>
-                                        <option value="3">Politics</option>
-                                        <option value="4">Health</option>
+                                        <option value="BBC-world">World</option>
+                                        <option value="BBC-business">Business</option>
+                                        <option value="BBC-politics">Politics</option>
+                                        <option value="BBC-health">Health</option>
                                     </select>
 
                                     <br/>
@@ -88,12 +92,12 @@ export class Vaihtoehdot extends React.Component {
                                     <button><img src={HSlogo} width="40vw" height="40vh"/></button>
                                      <select className="mdb-select">
                                         <option value="" disabled selected>Pick your favorites</option>
-                                        <option value="1">Latest</option>
-                                        <option value="2">Homeland</option>
-                                        <option value="3">World</option>
-                                        <option value="4">Economy</option>
-                                        <option value="5">Politics</option>
-                                        <option value="6">Sports</option>
+                                        <option value="HS-tuoreimmat">Latest</option>
+                                        <option value="HS-kotimaa">Homeland</option>
+                                        <option value="HS-ulkomaat">World</option>
+                                        <option value="HS-talous">Economy</option>
+                                        <option value="HS-politiikka">Politics</option>
+                                        <option value="HS-urheilu">Sports</option>
 
                                     </select>
 
@@ -108,13 +112,13 @@ export class Vaihtoehdot extends React.Component {
                                     <button><img src={ISlogo} width="40vw" height="40vh"/></button>
                                     <select className="mdb-select">
                                         <option value="" disabled selected>Pick your favorites</option>
-                                        <option value="1">Latest</option>
-                                        <option value="2">Homeland</option>
-                                        <option value="3">World</option>
-                                        <option value="4">Economy</option>
-                                        <option value="5">Entertainment</option>
-                                        <option value="6">Music</option>
-                                        <option value="7">Sports</option>
+                                        <option value="IS-tuoreimmat">Latest</option>
+                                        <option value="IS-kotimaa">Homeland</option>
+                                        <option value="IS-ulkomaat">World</option>
+                                        <option value="IS-taloussanomat">Economy</option>
+                                        <option value="IS-viihde">Entertainment</option>
+                                        <option value="IS-musiikki">Music</option>
+                                        <option value="IS-urheilu">Sports</option>
 
                                     </select>
                                     <br/>
@@ -131,7 +135,7 @@ export class Vaihtoehdot extends React.Component {
                         <input type="submit" value="Clear"/>
                         <input type="submit" value="Confirm"/>
                     </form>
-
+                    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet"/>
                 </div>
 
 
