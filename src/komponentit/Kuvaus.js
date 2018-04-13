fa3d58db79be2257d0e3956ddd9602ab47f8e345
WaltteriBroder <waltteri.broder@hotmail.com>
2018-04-13 11:15:48 +0300

    tekstin viilaus

--- src/komponentit/Kuvaus.js
+++ src/komponentit/Kuvaus.js
@@ -7,7 +7,7 @@ export class Kuvaus extends Component {
             <div className="kuvaus">
                 <br/>
                 <p><b>With <font color="#0099ff">Need4Feed</font> you can save your preferences </b></p>
-                <p><b>and and find content YOU actually care about!</b></p>
+                <p><b>and find content YOU actually care about!</b></p>
                 <p><b>Just <a href={"/register"}> SIGN UP </a> and build </b></p>
                 <p></p>
                 <br/>
